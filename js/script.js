d23b237a7d47bbac3858fb343c527b6cce3c267d
Daniel Angulo <dangulo4@gmail.com>
2023-02-10 21:28:06 -0700

    submission

--- js/script.js
+++ js/script.js
@@ -3,3 +3,125 @@
 // function theMonth()
 // let summerMonth
 // let myLibraryFunction
+
+// STEP 2
+// 2.4              numeric literal expression
+// 'Hello World'    string literal expression
+// true             Boolean literal expression
+// null             null literal expression
+
+// STEP 3
+// let myExpression = (2-1) *1 -1
+
+// STEP 4
+// let firtName
+// let lastName
+// let address
+// let city
+// let state
+// let zipCode
+// let yourAge
+// let referralSource
+// let blnMayWeContactYou
+
+// STEP 5
+// let firstName = 'Daniel'
+// let lastName = 'Angulo'
+// let blnMayWeContactYou = true
+
+// STEP 6
+// let yourAge = 35;
+// alert('My age is ' + yourAge + 'years old');
+
+// STEP 7
+// let blnMayWeContactYou = true;
+// alert('Set the variable to ' + blnMayWeContactYou);
+// let days_until = 10;
+// let blnMayWeContactYou = false
+// alert('There are ' + days_until + blnMayWeContactYou + 'days till we pass the class!')
+
+// STEP 8
+// let someString =
+//   'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// alert(someString);
+// The following string is invalid as it is, I added a backslask to denote that the qoute in the string is a literal character and not an end-of-string terminator.
+
+// STEP 9
+// let x = null;
+// let y;
+// console.log(`x = ${x}`);
+// console.log(`y = ${y}`);
+
+// STEP 10
+// x = 'Hello';
+// console.log(typeof x);
+// y = 2;
+// console.log(typeof y);
+// z = true;
+// console.log(typeof z);
+// someObject = null;
+// console.log(typeof someObject);
+// let someNULL;
+// console.log(typeof someNULL);
+
+// STEP 11
+// let fname = 'Zak';
+// let lname = 'Ruvalcaba';
+// alert('Hello ' + fname + '' + lname + ', welcome to the JavaScript class!');
+
+// STEP 12
+// let fname = 'Daniel';
+// let lname = 'Angulo';
+// alert('Hello ' + fname + ' ' + lname + ', welcome to the JavaScript class!');
+
+// STEP 13
+// let greeting = 'Welcome to ';
+// let fullName = ' Daniel Angulo';
+// let course = 'JavaScript';
+// alert(greeting + course + fullName);
+
+// STEP 14
+// let greeting = 'Welcome to the JavaScript class!';
+// let fullName = 'Zak Ruvalcaba';
+// alert(`Hello ${fullName}.\n${greeting}`);
+
+// STEP 15
+// let fullName = prompt('Enter your first and last name');
+// let greeting = 'Welcome to the JavaScript class!';
+// alert(`Hello ${fullName}.\n${greeting}`);
+
+// STEP 16
+// let fullName = prompt('Enter your first and last name');
+// let course = prompt('What course are you taking?');
+// let greeting = `Welcome to the ${course} class!`;
+// alert(`Hello ${fullName}.\n${greeting}`);
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// total = x + y;
+// console.log(`total: ${total}`);
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+// let x = 60 / 3;
+// console.log(x);
+
+// STEP 21
+// x = 20;
+// y = 40 / 2;
+// console.log(x == y);
+
+// STEP 22
+// let someValue = null;
+// let someOtherValue;
+// console.log(someValue === someOtherValue);
